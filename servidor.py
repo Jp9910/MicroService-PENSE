9e0c08a04684d8c243b1ca45bd12fc4f4f1b1bff
IPv6.br <ipv6br@ipv6br.(none)>
2022-05-28 20:18:33 -0300

    servidor com requisicao e resposta http

--- servidor.py
+++ servidor.py
@@ -1,27 +1,32 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from socket import *
+import subprocess
 
 class Servidor(BaseHTTPRequestHandler):
     def do_GET(self):
+	processos = subprocess.Popen(["ps"], stdout=subprocess.PIPE)
+	output, error = processos.communicate()
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path))
+        self.wfile.write(bytes("<body>"))
+	self.wfile.write(bytes("<p>This is an example web server.</p>"))
+	self.wfile.write(bytes("<p> Processos rodando no servidor: %s</p>" % output))
+	self.wfile.write(bytes("<p>%s</p>" % error))
+        self.wfile.write(bytes("</body></html>"))
 
 portaServidor = 55504
 
-socketServidor = socket(AF_INET,SOCK_STREAM)
-ipservidorufs = '200.17.121.13'
-socketServidor.bind(('',portaServidor))
-socketServidor.listen(1)
+#socketServidor = socket(AF_INET,SOCK_STREAM)
+#ipservidorufs = '200.17.121.13'
+#socketServidor.bind(('',portaServidor))
+#socketServidor.listen(1)
 
 if __name__ == "__main__":        
-    webServer = HTTPServer(('localhost', portaServidor), Servidor)
-    print("Server started http://%s:%s" % ('localhost', portaServidor))
+    webServer = HTTPServer(("localhost", portaServidor), Servidor)
+    print("Server started http://%s:%s" % ("localhost", portaServidor))
 
     #try:
     webServer.serve_forever()
