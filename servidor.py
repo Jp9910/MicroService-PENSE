b35ba048984d7a945e6760f8ea2e448a2e99d91e
Joao Secundo <alundra@academico.ufs.br>
2022-05-28 20:39:46 -0300

    tabela de processos

--- servidor.py
+++ servidor.py
@@ -1,28 +1,53 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from socket import *
 import subprocess
-
 class Servidor(BaseHTTPRequestHandler):
     def do_GET(self):
-	processos = subprocess.Popen(["ps"], stdout=subprocess.PIPE)
-	output, error = processos.communicate()
+        processos = subprocess.Popen(["ps"], stdout=subprocess.PIPE)
+        output, error = processos.communicate()
+        out = output.split()
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path))
+        self.wfile.write(bytes("<html><head><title>servidor http em python</title></head>"))
+        #self.wfile.write(bytes("<p>Request: %s</p>" % self.path))
         self.wfile.write(bytes("<body>"))
-	self.wfile.write(bytes("<p>This is an example web server.</p>"))
-	self.wfile.write(bytes("<p> Processos rodando no servidor: %s</p>" % output))
-	self.wfile.write(bytes("<p>%s</p>" % error))
+        self.wfile.write(bytes("<table>"))
+        
+        self.wfile.write(bytes("<thead>"))
+        self.wfile.write(bytes("<tr>"))
+        self.wfile.write(bytes("<th>%s" %out[0]))
+        self.wfile.write(bytes("</th>"))
+        self.wfile.write(bytes("<th>%s" %out[1]))
+        self.wfile.write(bytes("</th>"))
+        self.wfile.write(bytes("<th>%s" %out[2]))
+        self.wfile.write(bytes("</th>"))
+        self.wfile.write(bytes("<th>%s" %out[3]))
+        self.wfile.write(bytes("</th>"))
+        self.wfile.write(bytes("</tr>"))
+        self.wfile.write(bytes("</thead>"))
+        
+        self.wfile.write(bytes("<tbody>"))
+        for i in range(4, len(out), 4):
+            self.wfile.write(bytes("<tr>"))
+            self.wfile.write(bytes("<td>%s" %out[i]))
+            self.wfile.write(bytes("</td>"))
+            self.wfile.write(bytes("<td>%s" %out[i+1]))
+            self.wfile.write(bytes("</td>"))
+            self.wfile.write(bytes("<td>%s" %out[i+2]))
+            self.wfile.write(bytes("</td>"))
+            self.wfile.write(bytes("<td>%s" %out[i+3]))
+            self.wfile.write(bytes("</td>"))
+            self.wfile.write(bytes("</tr>"))
+        self.wfile.write(bytes("</tbody>"))
+        
+        self.wfile.write(bytes("</table>"))
+        self.wfile.write(bytes("<p>This is an example web server.</p>"))
+        #self.wfile.write(bytes("<p> Processos rodando no servidor: %s</p>" % output))
+        self.wfile.write(bytes("<p>Erro: %s</p>" % error))
         self.wfile.write(bytes("</body></html>"))
 
 portaServidor = 55504
-
-#socketServidor = socket(AF_INET,SOCK_STREAM)
-#ipservidorufs = '200.17.121.13'
-#socketServidor.bind(('',portaServidor))
-#socketServidor.listen(1)
 
 if __name__ == "__main__":        
     webServer = HTTPServer(("localhost", portaServidor), Servidor)
@@ -35,6 +60,11 @@ if __name__ == "__main__":
 
     webServer.server_close()
     print("Server stopped.")
+
+#socketServidor = socket(AF_INET,SOCK_STREAM)
+#ipservidorufs = '200.17.121.13'
+#socketServidor.bind(('',portaServidor))
+#socketServidor.listen(1)
 
 # while 1:
 #     print "O servidor esta pronto para receber"
